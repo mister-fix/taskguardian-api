7cc59ce3b606a416d842531a2aaab234c098f798
Stephen <hellostephenwm@gmail.com>
2024-09-14 01:46:55 -0400

    chore(plop): add plop configuration file

---
+++ plopfile.js
@@ -0,0 +1,5 @@
+// Plop generators configurations
+module.exports = function (/** @type {import('plop').NodePlopAPI} */ plop) {
+  // Welcome message
+  plop.setWelcomeMessage('Select a generator');
+};
